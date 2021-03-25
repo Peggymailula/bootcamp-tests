97a7dae487ca4816ef91f32f2353967705a37e91
Peggy <peggymailula@gmail.com>
2021-03-25 16:36:22 +0200

    update

--- test/isFromBellville.test.js
+++ test/isFromBellville.test.js
@@ -1,6 +1,6 @@
 describe('isFromBellville' , function(){
     it('returns true if a registration number is for Bellville otherwise returns false.' , function(){
-        assert.equal(isFromBellville('CY12PF'),tru);
+        assert.equal(isFromBellville('CY12PF'),true);
         assert.equal(isFromBellville('CJY12345'),false);
         assert.equal(isFromBellville('CYA099'),true);
 
