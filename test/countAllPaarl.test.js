e468a6515e4c1919b36a8888e318b411d39bfa0d
Peggy <peggymailula@gmail.com>
2021-03-29 10:13:49 +0200

    update

--- test/countAllPaarl.test.js
+++ test/countAllPaarl.test.js
@@ -1,16 +1,16 @@
 describe('countAllPaarl' , function(){
-    it('takes a parameter string with registration numbers separated with commas and returns the number of registration numbers in the string for Paarl.' , function(){
+    it('It should return the number of registration for paarl registrations in the string' , function(){
         assert.equal(countAllPaarl('CJ4565, CJ7830, CJ123'), 3 );
         
     });
 
-    it('takes a parameter string with registration numbers separated with commas and returns the number of registration numbers in the string for Paarl.' , function(){
+    it('It should return the number of registration for paarl registrations in the string' , function(){
         assert.equal(countAllPaarl('CA4565, CY7830, CJ123'), 1 );
        
         
     });
 
-    it('takes a parameter string with registration numbers separated with commas and returns the number of registration numbers in the string for Paarl.' , function(){
+    it('It should return the number of registration for paarl registrations in the string' , function(){
         assert.equal(countAllPaarl('CJ4565, CJ7830, CA123'), 2 );
 
         
--- test/findItemsOver.test.js
+++ test/findItemsOver.test.js
@@ -1,5 +1,5 @@
 describe('findItemsOver' , function(){
-    it('takes a list of objects each with a name and qty attribute. And also a second parameter which is a threshold for the quantity of items available. The function should return products that have quantity higher than the threshold.' , function(){
+    it('should return products that have quantity higher than the threshold..' , function(){
         assert.deepEqual(findItemsOver([
             {name : 'apples', qty : 10},{name : 'pears', qty : 37},{name : 'bananas', qty : 27},{name : 'apples', qty : 3}], 25)
        , [{name : 'pears', qty : 37},{name : 'bananas', qty : 27}]);
@@ -7,7 +7,7 @@ describe('findItemsOver' , function(){
     });
 
 
-    it('takes a list of objects each with a name and qty attribute. And also a second parameter which is a threshold for the quantity of items available. The function should return products that have quantity higher than the threshold.' , function(){
+    it('should return products that have quantity higher than the threshold.' , function(){
      
        assert.deepEqual(findItemsOver([
        {name : 'apples', qty : 10},{name : 'pears', qty : 37},{name : 'bananas', qty : 27},{name : 'apples', qty : 3}], 5)
@@ -16,7 +16,7 @@ describe('findItemsOver' , function(){
      
     });
 
-    it('takes a list of objects each with a name and qty attribute. And also a second parameter which is a threshold for the quantity of items available. The function should return products that have quantity higher than the threshold.' , function(){
+    it('should return products that have quantity higher than the threshold.' , function(){
        
 
        assert.deepEqual(findItemsOver([
